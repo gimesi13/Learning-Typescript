89c71cac172b0eb5e03643965886ea60739df446
Gergely Gimesi <105725861+gimesi13@users.noreply.github.com>
2022-10-13 14:40:53 +0300

    setting up basic styles

--- typescript-shopping-cart/src/App.tsx
+++ typescript-shopping-cart/src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+import { About } from "./pages/About";
+import { Navbar } from "./components/Navbar";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
