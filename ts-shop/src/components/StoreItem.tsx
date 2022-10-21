4754192aa0a49614a0b8ceca768dc0870bddc47b
Gergely Gimesi <105725861+gimesi13@users.noreply.github.com>
2022-10-21 16:27:31 +0300

    creating context

--- ts-shop/src/components/StoreItem.tsx
+++ ts-shop/src/components/StoreItem.tsx
@@ -8,6 +8,10 @@ type StoreItemProps = {
 };
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+  function getItemQuantity(id: number) {
+    return 2;
+  }
+  let quantity = getItemQuantity(id);
   return (
     <Card className="h-100">
       <Card.Img
@@ -21,6 +25,28 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
           <span className="fs-2">{name}</span>
           <span className="ms-2 text-muted">{price}</span>
         </Card.Title>
+        {quantity === 0 ? (
+          <Button onClick={() => {}}>+ Add to Cart</Button>
+        ) : (
+          <div
+            className="d-flex align-items-center flex-column"
+            style={{ gap: ".5rem" }}
+          >
+            <div
+              className="d-flex align-items-center justify-content-center"
+              style={{ gap: ".5rem" }}
+            >
+              <Button onClick={() => {}}>+</Button>
+              <div>
+                <span>{quantity}</span> in cart
+              </div>
+              <Button onClick={() => {}}>-</Button>
+            </div>
+            <Button variant="danger" onClick={() => {}}>
+              Remove
+            </Button>
+          </div>
+        )}
       </Card.Body>
     </Card>
   );
---
+++ ts-shop/src/context/ShoppingCartContext.tsx
@@ -0,0 +1,1 @@
+import { createContext, ReactNode, useContext, useState } from "react";
