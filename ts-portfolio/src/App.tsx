a8edab7eabf80bf3dc9637e0a41419606c38b89f
Gergely Gimesi <105725861+gimesi13@users.noreply.github.com>
2022-10-24 22:10:56 +0300

    i18next

--- ts-portfolio/src/App.tsx
+++ ts-portfolio/src/App.tsx
@@ -3,13 +3,38 @@ import { Suspense } from "react";
 import { MainPage } from "pages/MainPage";
 /* router */
 import { Route, Routes } from "react-router-dom";
+/* Translation package */
+import i18n from "i18next";
+import { initReactI18next, useTranslation } from "react-i18next";
+/* Languages */
+import En from "./languages/en.json";
+import Hu from "./languages/hu.json";
+
+/* LANGUAGES */
+const translationsEn = En;
+const translationsHu = Hu;
+
+i18n
+  .use(initReactI18next) //passes i18n down to react-i18next
+  .init({
+    resources: {
+      en: { translation: translationsEn },
+      hu: { translation: translationsHu },
+    },
+    lng: "en",
+    fallbackLng: "en",
+    interpolation: { escapeValue: false },
+  });
+/* LANGUAGES END */
 
 const App: React.FC = () => {
+  /* useTranslation hook */
+  const { t } = useTranslation();
   return (
     <Suspense fallback="Loading...">
       <div className="App">
         <Routes>
-          <Route path="/" element={<MainPage />}></Route>
+          <Route path="/" element={<MainPage num={2} t={t} />}></Route>
           {/* <Route path="/asd" element={<Another />}></Route>
           <Route path="/ds" element={<Other />}></Route> */}
         </Routes>
--- ts-portfolio/src/pages/MainPage.tsx
+++ ts-portfolio/src/pages/MainPage.tsx
@@ -5,8 +5,13 @@ import { Skills } from "../components/Skills";
 import { Projects } from "../components/Projects";
 import { Contact } from "../components/Contact";
 import { Footer } from "../components/Footer";
+import { WithT } from "i18next";
 
-export const MainPage = () => {
+interface Props {
+  num: number;
+}
+
+export const MainPage: React.FC<Props & WithT> = ({ t, num }): JSX.Element => {
   return (
     <>
       <NavBar />
